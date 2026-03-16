79f6d02086d5a907e0274cb2e9679678a85de0a5
Lunes <julesmusquin@protonmail.com>
2026-03-16 09:55:43 +0100

    documentation test

--- app/routes/generales.py
+++ app/routes/generales.py
@@ -3,7 +3,11 @@ from flask import render_template
 from ..models.models import InstitutionDefinitive
 
 @app.route('/')
-def home():
+def test():
+    '''
+    Test est une fonction de test de connection entre l'application, l'ORM et la base de donnée postgres.
+     Il s'agit d'un simple SELECT * (query.all()) affiché au sein d'un htlm brut.
+    '''
     donnees = []
     for institution in InstitutionDefinitive.query.all():
         donnees.append({
